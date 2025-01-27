d8289305350f0362fd77183cb4c8dc7ef28ede89
Diego Portillo <dapadxdxd@gmail.com>
2025-01-27 21:03:30 +0100

    oopsie

--- rohan/configuration/config.py
+++ rohan/configuration/config.py
@@ -1,5 +1,5 @@
 import os
 
-API_URL = os.environ.get("API_URL", "http://192.168.0.20:8081")
+API_URL = os.environ.get("API_URL", "http://192.168.0.21:8081")
 DURATION = os.environ.get("DURATION", 10)
 CONFIG_FILE = os.environ.get("CONFIG_FILE", "cage_config.json")
